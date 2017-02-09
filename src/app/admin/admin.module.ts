b13daed16843e6398ca24d052ce632c9eb585d8d
hhasandt <hhasan@dohatec.net>
2017-02-09 11:47:12 +0600

    v6

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ChangepassComponent } from './changepass/changepass.component';
 import { ChangeprofileComponent } from './changeprofile/changeprofile.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import {AdminRouting} from "./admin-routing.module";
-
+import { AdminheaderComponent } from './adminheader/adminheader.component';
+import { AdminsidebarComponent } from './adminsidebar/adminsidebar.component';
+import { AdminRouting} from "./admin-routing.module";
 @NgModule({
   imports: [
     CommonModule,AdminRouting
   ],
-  declarations: [ChangepassComponent, ChangeprofileComponent, DashboardComponent],
-  exports:[AdminRouting]
+  declarations: [ChangepassComponent, ChangeprofileComponent, DashboardComponent, AdminheaderComponent, AdminsidebarComponent],
+  exports:[AdminRouting,AdminheaderComponent,AdminsidebarComponent]
 })
 export class AdminModule { }
